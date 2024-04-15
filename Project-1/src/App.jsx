cafedb8ea9e29610ed1673df8d2b1b3f64c6ae11
MrYaseen03 <freelancewebskills@gmail.com>
2024-04-16 00:08:46 +0500

    First Project done

---
+++ Project-1/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+import Nav from "./components/Nav"
+import HeroSection from "./components/Hero.Section"
+const App = ()=>{
+    return (
+    <div>
+        <Nav/>
+        <HeroSection/>
+    </div>
+)
+}
+
+export default App
