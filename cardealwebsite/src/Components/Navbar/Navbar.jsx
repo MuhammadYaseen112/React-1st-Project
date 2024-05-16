1b214722112ba3e5ff935d58735f2ac8cb7eac97
MrYaseen03 <freelancewebskills@gmail.com>
2024-05-16 22:39:36 +0500

    Importing all files in App jsx

--- cardealwebsite/src/Components/Navbar/Navbar.jsx
+++ cardealwebsite/src/Components/Navbar/Navbar.jsx
@@ -2,8 +2,29 @@ import './Navbar.css';
 
 const Navbar = ()=>{
     return(
-        <>
-        </>
+        <div className="header">
+            <div className="logo">
+                <img src="" alt="logo" className='logo' />
+            </div>
+            <div className="navbar">
+                <ul className="menu">
+                    <li className="listItem">
+                        <a href="#" className='link'>Used Cars</a>
+                    </li>
+                    <li className="listItem">
+                        <a href="#" className='link'>New Cars</a>
+                    </li>
+                    <li className="listItem">
+                        <a href="#" className='link'>Auctions</a>
+                    </li>
+                    <li className="listItem">
+                        <a href="#" className='link'>Sell</a>
+                    </li>
+                </ul>
+            </div>
+
+
+        </div>
     )
 }
 
